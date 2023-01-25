542ea48115b4f9566c6a14f94fe79a166ee420b5
JamilRayhan <jamilrayhan.cse@gmail.com>
2023-01-25 19:18:32 +0600

    first time

--- map.py
+++ map.py
@@ -1,10 +1,11 @@
 # map function
 def func(n):
-    return n*n*n
+    return n * n * n
+
 
 l = [3, 4, 1, 0, 6]
 
-newL = list(map(lambda n: n*n*n, l)) # newL = [n*n*n for n in l]
+newL = list(map(lambda n: n * n * n, l))  # newL = [n*n*n for n in l]
 
 l = ['Simanta', 'Bohubrihi', 'Dhaka']
 
